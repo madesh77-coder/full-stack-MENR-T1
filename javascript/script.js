b3cd8c1154050a47d7111e8cdaeb0a64a3d33f90
madesh <madeshganesh77>
2026-06-05 14:17:28 +0530

    js completed

---
+++ javascript/script.js
@@ -0,0 +1,186 @@
+// a=20;
+// b="30";
+
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a>=b);
+// console.log(a==b);
+// console.log(a!=b);
+
+
+// var a =10;
+
+// console.log(a--);
+// console.log(--a);
+
+//  var a = 10;
+//  var b = 20;
+// if(true){
+//     console.log('$ {a} + & {b} =  $ {a+b}')
+// }
+
+// var a = 10;
+// const b = 20;
+// if(true){
+//     console.log(a);
+//     console.log(b);
+// } 
+// console.log(a);
+// console.log(b);
+
+// var a =10;
+// if(a%2==0){
+//     console.log('$ {a} is even');
+
+// }
+//  else{
+//     console.log('${b} is odd');
+
+//  }
+
+
+//  var mark =30;
+//  if (mark>=75){
+//     console.log("A grade");
+//  }
+//  else if(mark>=35){
+//     console.log("pass")
+//  }
+//  else{
+//     console.log("fail")
+//  }
+
+//  mark =30;
+//  var result=(mark>=90) ? "o grade" :
+//             (mark>=70) ? "a grade" :
+//             (mark>=35) ? "pass":
+//             "fail";
+// console.log (result);
+
+//    var day = 8;
+//    switch(day){
+//     case 1:{
+//         console.log("sunday");
+//         break;
+//     }
+//     case 2:{
+//         console.log("monday");
+//         break;
+//     }          
+//     case 3:{
+//         console.log("tuesday");
+//         break;
+//     }
+//     case 4:{
+//         console.log("friday");
+//     }
+//     default:{
+//         console.log("invalid input");
+//     }
+//    }
+
+//    looping statement
+
+//  1times 11 times
+
+// for(var i=1;i<=19;i++){
+//     console.log(i);
+// }
+
+// var val =248;
+// var count=0;
+// while(val > 0){
+//     count++;
+//     val =  Math.floor(val/10)
+// }
+// console.log(Count)
+
+// for(let i=1;i<=10;i++){
+//     if(i%2===0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// add ();
+
+//  function add (){
+//     console.log(10+20);
+//  } 
+
+//  add() //hoisting
+   
+//  function add(a,b){
+//     console.log(a+b);
+//  }
+
+//  add(10,20);
+
+//  function add(a=5 , b=6){
+//     console.log(a+b);
+//  } 
+//  add();
+
+// 
+
+//spread operater
+
+// var arr =[10,20];
+// var arr2 =[arr,40,50];
+// console.log(arr);
+// console.log(arr2);
+
+//destructuring operater
+
+// var[m1,m2,m3,m4,m5,]=[90,99,98,97,100]
+// console.log(m1,m2,m3,m4,m5)
+
+// var {name,mobile,dept,email,isactive}={
+//     name:"vikram",
+//     mobile:1234567890,
+//     dept:["ct","aids"],
+//     email:"dfcsbghgfj.com",
+//     isactive:true
+// }
+// console.log(name,mobile,email,dept,isactive)
+
+var obj ={
+    name:"maff",
+    dept:[sd],
+    mobile:2323425565
+}
+for(Let key in obj){
+    console.log(key,obj[key])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
